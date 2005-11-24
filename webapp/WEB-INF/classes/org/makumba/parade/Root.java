b5a2e461469b582c3c0d4f4e3d2c9b70028e0ad6
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-11-24 20:13:22 +0000

    Added methods for getting and writing data to persistant manager classes

--- webapp/WEB-INF/classes/org/makumba/parade/Root.java
+++ webapp/WEB-INF/classes/org/makumba/parade/Root.java
@@ -8,40 +8,47 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
 
-
 import org.apache.log4j.Logger;
+
 
 
 public class Root {
 
-	static Logger logger = Logger.getLogger(Root.class.getName());
-
-    private static Configuration cfg;
+	private static Logger logger = Logger.getLogger(Root.class.getName());
+    
+	private static Configuration cfg;
     
     static final Long one = new Long(1);
     
-    private static Root root =null;
+    private static Root root = null;
+    
+    private static SessionFactory sessionFactory;
 
-    private SessionFactory sessionFactory;
+    private static Session session = null;
     
-    private Session session = null;
+    private Manager mgr = new Manager();
     
-    public void init() {
+    public void initRoot() {
 
         try {
-
-            session = sessionFactory.openSession();
-            
-            Transaction tx = session.beginTransaction();
+        	
+        	session = sessionFactory.openSession();
+        	
+        	Transaction tx = session.beginTransaction();
 
             Iterator i = session.createQuery("FROM Parade p").iterate();
+            
             if (!i.hasNext()) {
                 Parade p = new Parade();
                 p.setId(one);
-                session.save(p);
+                session.saveOrUpdate(p);
             }
             
             tx.commit();
+            
+            mgr.init();
+            
+            
             
         } catch (Throwable t) {
             logger.error(t); t.printStackTrace();
@@ -59,22 +66,52 @@ public class Root {
     	
     }
 	
-	private Root getParade() {
-        return (Root)session.get(Parade.class, one);
-    }
-	
-	private void setSessionFactory(SessionFactory sf) {
-		this.sessionFactory = sf;
-	}
-	
-	/* For later use, once pluggable session managment works fine
-	
-	private Session session() {
-		//return this.sessionFactory.getCurrentSession();
+	private static void createPersistentManager(String className, Class type) {
 		
+		Transaction tx = session.beginTransaction();
+		
+		Iterator i = session.createQuery("FROM "+className+" c").iterate();
+		if (!i.hasNext()) {
+			
+			try {
+				
+				Object o = Class.forName(className).asSubclass(Manager.class).newInstance();
+				Manager c = (Manager) o;
+				
+				c.setId(new Long(1));
+				session.saveOrUpdate(c);
+			} catch (Throwable t) {
+				logger.error(t); t.printStackTrace();
+			}
+			tx.commit();
+			
+        }
 	}
 	
-	*/
+	public static ManagerIfc getManagerData(String className, Class type) {
+		
+		createPersistentManager(className,type);
+		
+		Transaction tx = session.beginTransaction();
+		
+		ManagerIfc data = (ManagerIfc) session.get(className, new Long(1));
+		if (data == null) {
+            logger.error("Cannot get manager data");
+		}
+		
+		tx.commit();
+		
+		return data;
+	}
+	
+	public static void setManagerData(ManagerIfc data) {
+		Transaction tx = session.beginTransaction();
+		
+		session.saveOrUpdate(data);
+		
+		tx.commit();
+	}
+	
 	
 	public static Root getInstance() throws Exception {
 	    if (root==null){
@@ -96,12 +133,13 @@ public class Root {
             cfg.addResource("../conf/FileHierarchy.hbm.xml");
             cfg.addResource("../conf/FileDataHierarchy.hbm.xml");
             
-            setSessionFactory(cfg.buildSessionFactory());
+            sessionFactory = cfg.buildSessionFactory();
             
             // making hibernate creating the tables
             SchemaUpdate schemaUpdate = new SchemaUpdate(cfg);
             schemaUpdate.execute(true, true);
             
+
         } catch (Throwable t) {
             logger.error(t); t.printStackTrace();
         }

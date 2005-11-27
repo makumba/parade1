f1e781e70d9a8214f75a61b61ea9e2f07f98bf50
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-11-27 17:05:59 +0000

    Moved to model package

---
+++ webapp/WEB-INF/classes/org/makumba/parade/model/CvsManager.java
@@ -0,0 +1,17 @@
+package org.makumba.parade.model;
+
+
+public class CvsManager extends FileData {
+	
+	public Long id;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+
+}

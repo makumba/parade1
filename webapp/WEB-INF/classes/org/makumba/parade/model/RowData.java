993adcaa5d76e95dfd69646dce247be83c637909
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-11-28 10:01:48 +0000

    New fields: rowpath, rowdescription

---
+++ webapp/WEB-INF/classes/org/makumba/parade/model/RowData.java
@@ -0,0 +1,73 @@
+package org.makumba.parade.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.io.File;
+
+public class RowData {
+	
+	public Long id;
+	
+	public String rowname;
+	
+	public String rowpath;
+	
+	public String description;
+	
+	private Map managers = new HashMap();
+	
+	private Map files = new HashMap();
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public RowData() {
+		
+	}
+
+	public String getRowname() {
+		return rowname;
+	}
+
+	public void setRowname(String rowname) {
+		this.rowname = rowname;
+	}
+	
+	public Map getManagers() {
+		return managers;
+	}
+
+	public void setManagers(Map managers) {
+		this.managers = managers;
+	}
+
+	public Map getFiles() {
+		return files;
+	}
+
+	public void setFiles(Map files) {
+		this.files = files;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getRowpath() {
+		return rowpath;
+	}
+
+	public void setRowpath(String rowpath) {
+		this.rowpath = rowpath;
+	}
+
+}

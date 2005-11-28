513944ea3fb8d19be73d00dd52f7506c9436b247
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-11-28 10:07:18 +0000

    Changed initialization, new data access methods

--- webapp/WEB-INF/classes/org/makumba/parade/Root.java
+++ webapp/WEB-INF/classes/org/makumba/parade/Root.java
@@ -2,15 +2,18 @@ package org.makumba.parade;
 
 import java.util.*;
 
+import javax.servlet.jsp.PageContext;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
-import org.makumba.parade.model.Parade;
+import org.makumba.parade.model.ManagerData;
+import org.makumba.parade.model.ParadeData;
+import org.makumba.parade.model.RowData;
 
 import org.apache.log4j.Logger;
-
 
 
 public class Root {
@@ -27,33 +30,24 @@ public class Root {
 
     private static Session session = null;
     
+    private static ParadeData p;
+    
+    private static ManagerData m;
+    
+    private static RowData r;
+    
     private Manager mgr = new Manager();
     
     public void initRoot() {
-
-        try {
         	
-        	session = sessionFactory.openSession();
-        	
-        	Transaction tx = session.beginTransaction();
-
-            Iterator i = session.createQuery("FROM Parade p").iterate();
-            
-            if (!i.hasNext()) {
-                Parade p = new Parade();
-                p.setId(one);
-                session.saveOrUpdate(p);
-            }
-            
-            tx.commit();
-            
-            mgr.init();
-            
-            
-            
-        } catch (Throwable t) {
-            logger.error(t); t.printStackTrace();
-        }
+       	getParadeData();
+       	//getRowData();
+       	
+        mgr.init();
+        mgr.readManagers();
+        mgr.initManagers();
+        
+        close();
 
     }
     
@@ -113,6 +107,120 @@ public class Root {
 		tx.commit();
 	}
 	
+	public static ManagerData getManager() {
+		Transaction tx = session.beginTransaction();
+		
+		Iterator i = session.createQuery("FROM ManagerData m").iterate();
+		
+		if (!i.hasNext()) {
+			
+			try {
+				m = new ManagerData();
+				m.setId(new Long(1));
+				session.saveOrUpdate(m);
+			} catch (Throwable t) {
+				logger.error(t); t.printStackTrace();
+			}
+			tx.commit();
+		} else {
+			m = (ManagerData) i.next();
+		}
+			
+			return m;
+	}
+	
+	public static void setManagerData(ManagerData m) {
+		Transaction tx = session.beginTransaction();
+		
+		m.setCommon(m.getCommon());
+		
+		session.saveOrUpdate(m);
+		
+		tx.commit();
+	}
+	
+	public static void getParadeData() {
+		Transaction tx = session.beginTransaction();
+
+        try {     	
+       	
+        	Iterator i = session.createQuery("FROM ParadeData p").iterate();
+            
+            if (!i.hasNext()) {
+                p = new ParadeData();
+                p.setId(one);
+                session.saveOrUpdate(p);
+                tx.commit();
+            } else {
+            	p = (ParadeData) i.next();
+            }
+            
+        } catch (Throwable t) {
+            logger.error(t); t.printStackTrace();
+        }
+        
+	}
+	
+	public static void setParadeData(ParadeData data) {
+		Transaction tx = session.beginTransaction();
+		
+		p.setRows(data.getRows());
+		
+		session.saveOrUpdate(p);
+		
+		tx.commit();
+	}
+	
+	/* Return Parade rows
+	 * TODO: implement it
+	 */
+	public static Map getParadeRows() {
+		return null;
+	}
+	
+	/* Set Parade rows */
+	public static void setParadeRows(Map rows) {
+		Transaction tx = session.beginTransaction();
+		
+		p.setRows(rows);
+		
+		session.saveOrUpdate(p);
+		
+		tx.commit();
+		
+	}
+	
+	public static void getRowData() {
+		Transaction tx = session.beginTransaction();
+
+        try {     	
+       	
+        	Iterator i = session.createQuery("FROM RowData r").iterate();
+            
+            if (!i.hasNext()) {
+                r = new RowData();
+                r.setId(one);
+                session.saveOrUpdate(r);
+                tx.commit();
+            } else {
+            	r = (RowData) i.next();
+            }
+            
+        } catch (Throwable t) {
+            logger.error(t); t.printStackTrace();
+        }
+	}
+	
+	public static void setRowData(RowData data) {
+		Transaction tx = session.beginTransaction();
+		
+		//do something useful
+		
+		session.saveOrUpdate(p);
+		
+		tx.commit();
+	}
+	
 	
 	public static Root getInstance() throws Exception {
 	    if (root==null){
@@ -126,8 +234,8 @@ public class Root {
         try {
             
             cfg = new Configuration().configure("../conf/hibernate.cfg.xml");
-            cfg.addResource("../conf/Parade.hbm.xml");
-            cfg.addResource("../conf/Row.hbm.xml");
+            cfg.addResource("../conf/ParadeData.hbm.xml");
+            cfg.addResource("../conf/RowData.hbm.xml");
             cfg.addResource("../conf/ManagerData.hbm.xml");
             cfg.addResource("../conf/ManagerDataHierarchy.hbm.xml");
             cfg.addResource("../conf/File.hbm.xml");
@@ -139,6 +247,8 @@ public class Root {
             // making hibernate creating the tables
             SchemaUpdate schemaUpdate = new SchemaUpdate(cfg);
             schemaUpdate.execute(true, true);
+            
+            session = sessionFactory.openSession();
             
 
         } catch (Throwable t) {
